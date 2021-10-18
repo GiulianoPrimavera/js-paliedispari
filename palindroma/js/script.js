e9211ce9e199a485ecac5e5d95a38c3e65b134a1
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-18 17:12:46 +0200

    adds the function reverse to reverse the array, adds the function join to join the elements of the reverted array, adds an if to see if the word is palindrome

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,12 +1,27 @@
 //chiedo all'utente di inserire una parola che poi salvo in una variabile
 const parolaInserita = prompt("inserisci una parola a tua scelta");
+console.log("la parola inserita è ", parolaInserita);
 
 function isPalindroma(parola){
     //uso la funzione split per dividere la parola in lettere che vengono inserite in un array (in questo caso chiamato "spezzettaLaStringa")
     let spezzettaLaStringa = parola.split("");
-    console.log(spezzettaLaStringa);
+    console.log("la parola in array è ", spezzettaLaStringa);
     
+    //inverto l'ordine degli elementi all'interno dell'array appena creato
+    let invertoLArray = spezzettaLaStringa.reverse();
+    console.log("l'array invertito è ", invertoLArray);
+    
+    //collego gli elementi dell'array che ora è invertito in una nuova "parola"
+    let parolaInvertita = spezzettaLaStringa.join("");
+    console.log("la parola invertita è ", parolaInvertita);
 
+    if(parola === parolaInvertita){
+        alert("questa parola è palindroma");
+        console.log("questa parola è palindroma");
+    }else{
+        alert("questa parola non è palindroma");
+        console.log("questa parola non è palindroma");
+    }
 }
 
 isPalindroma(parolaInserita);

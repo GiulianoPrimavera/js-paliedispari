cf32306119becd7225c41164b53522cc09890832
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-18 16:47:28 +0200

    adds the function that finds the addition of the two numbers and decide a winner,

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -6,12 +6,13 @@ console.log(inserisciPariODispari);
 while(inserisciPariODispari === undefined){
 
     inserisciPariODispari = prompt("scegli pari o dispari?")
+    inserisciPariODispari.toLowerCase();
 
-    if(inserisciPariODispari.toLowerCase() === "pari"){
+    if(inserisciPariODispari === "pari"){
         alert("hai scelto pari");
         console.log("hai scelto pari");
 
-    }else if(inserisciPariODispari.toLowerCase() === "dispari"){
+    }else if(inserisciPariODispari === "dispari"){
         alert("hai scelto dispari");
         console.log("hai scelto dispari");
     } else{
@@ -19,7 +20,7 @@ while(inserisciPariODispari === undefined){
         inserisciPariODispari = undefined;
     }
 }
-console.log(inserisciPariODispari);
+console.log("l'utente ha scelto ", inserisciPariODispari);
 
 
 //chiedi all'utente di inserire un numero tra 1 e 5
@@ -42,14 +43,38 @@ while(numeroDellUtente === undefined || isNaN(numeroDellUtente)){
 
 //il computer genera un numero randomtra 1 e 5
 const numeroDelComputer = Math.ceil(Math.random()* 5)
+alert("il computer ha scelto " + numeroDelComputer);
 console.log("numero del computer ", numeroDelComputer);
 
 //sommiamo i due numeri
 const sommaDeiDueNumeri = numeroDellUtente + numeroDelComputer;
+alert("la somma dei due numeri è " + sommaDeiDueNumeri);
+console.log(sommaDeiDueNumeri);
 
 //stabiliamo se la somma dei due numeri è pari o dispari
-function getPariDispari()){
+function getPariDispari(num1, num2){
+    let risultato = num1 + num2;
 
+    //con questo if controllo se la somma dei due numeri è pari o dispari, se è pari...
+    if(risultato % 2 === 0){
+        //qui controllo cosa ha inserito l'utente: 
+        if(inserisciPariODispari === "pari"){
+            alert("utente vince");
+            console.log("utente vince");
+        }else {
+            alert("computer vince")
+            console.log("computer vince")
+        }
+    }else{
+        if (inserisciPariODispari === "dispari"){
+            alert("utente vince")
+            console.log("utente vince")
+        }else{
+            alert("computer vince")
+            console.log("computer vince")
+        }
+    }
+   /*  return risultato;  */  
 }
 
-//dichiariamo se l'utente a vinto o no (se ha perso, vuol dire che ha vinto il computer)
+getPariDispari(numeroDellUtente, numeroDelComputer);
